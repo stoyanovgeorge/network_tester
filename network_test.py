0da36cc4f1cc53d933988e3a25d31330ca848b91
George Stoyanov <gstoyanov@gmail.com>
2018-12-05 06:50:39 +0000

    fixed the multiline comment to triple quoation mark

--- network_test.py
+++ network_test.py
@@ -4,9 +4,9 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 speedtest_log = os.path.join(basedir, 'speedtest_log.csv')
 
 def curr_date():
-    ``` Returns a list containing the date as a first
-    component and the time as a second component. The 
-    format of the date is DD-MM-YYYY```
+    """Returns a list containing the date as a first
+    component and the time as a second component. 
+    The format of the date is DD-MM-YYYY"""
     now = datetime.datetime.now()
     now_date = now.strftime("%d-%m-%Y")
     now_time = now.strftime("%H:%M")
@@ -14,8 +14,8 @@ def curr_date():
 
 
 def nettest():
-    ``` Returns a list containing the date, time, ping time
-    download speed and upload speed from the best server```
+    """Returns a list containing the date, time, ping time
+    download speed and upload speed from the best server"""
     cmd = "speedtest-cli --simple"
     result = subprocess.check_output(shlex.split(cmd))
     result = result.decode('ascii').strip()
@@ -27,10 +27,10 @@ def nettest():
     return res_list
 
 def csv_write(lst):
-    ``` Checks if the speedtest_log file exists, if it doesn't
+    """Checks if the speedtest_log file exists, if it doesn't
     it creates it and adds the header row labesl, if it does, 
     it will append the result to the end adding a new row. The
-    CSV delimiter is set to be ';' which works well with Excel```
+    CSV delimiter is set to be ';' which works well with Excel"""
     if os.path.isfile(speedtest_log):
 	with open(speedtest_log, "a") as log:
             writer = csv.writer(log, delimiter=';')
