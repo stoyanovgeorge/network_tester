d24536947b2bd631111ef3f21842dde8806796d5
George Stoyanov <gstoyanov@gmail.com>
2018-12-05 06:43:50 +0000

    adding a README.MD and comments to the Python script

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# network_tester
-Simple Python script to test the network speed based on speedtest-cli. 
+# Network Tester
+
+### Introduction
+
+This is a simple Python script to test the network speed based on speedtest-cli. It records the current date, time and the ping, download and upload speed in a `speedtest_log.csv` file in the same directory. In case that the `speedtest_log.csv` doesn't exist it will be created otherwise it will just append the results to the end of the file, adding new rows. 
+
+### Configuration 
+
+You can also define the server using the `--server` option to the `speedtest-cli` command. You can get a list of all available servers sorted by their distance to you by executing: 
+```
+$ speedtest-cli --list > speedtest_servers.txt
+```
+From this list you can select your own server and add its ID in the `cmd`:
+
+```
+speedtest-cli --server ID --simple
+```
+
+where ID is the numerical ID of the selected server. This will force `speedtest-cli` to use every time this server. 
+
+### Automation of the Script
+
+In order to run it continuously you can add an entry to the crontab and the interval at each to make the poll like:
+```
+*/10 * * * * python3 /home/<username>/<path_to_the_script>/network_test.py
+``` 
+This means that the script will run every 10 minutes. 
--- network_test.py
+++ network_test.py
@@ -4,34 +4,41 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 speedtest_log = os.path.join(basedir, 'speedtest_log.csv')
 
 def curr_date():
-	now = datetime.datetime.now()
-	now_date = now.strftime("%d-%m-%Y")
-	now_time = now.strftime("%H:%M")
-	return [now_date, now_time]
+    ``` Returns a list containing the date as a first
+    component and the time as a second component. The 
+    format of the date is DD-MM-YYYY```
+    now = datetime.datetime.now()
+    now_date = now.strftime("%d-%m-%Y")
+    now_time = now.strftime("%H:%M")
+    return [now_date, now_time]
 
 
 def nettest():
-	cmd = "speedtest-cli --simple"
-	result = subprocess.check_output(shlex.split(cmd))
-	result = result.decode('ascii').strip()
-	lines = result.split('\n')
-	res_list = curr_date()
-	for line in lines:
-		res_list.append(line.split(":")[1].strip())
+    ``` Returns a list containing the date, time, ping time
+    download speed and upload speed from the best server```
+    cmd = "speedtest-cli --simple"
+    result = subprocess.check_output(shlex.split(cmd))
+    result = result.decode('ascii').strip()
+    lines = result.split('\n')
+    res_list = curr_date()
+    for line in lines:
+	res_list.append(line.split(":")[1].strip())
 
-	return res_list
+    return res_list
 
 def csv_write(lst):
-	if os.path.isfile(speedtest_log):
-		with open(speedtest_log, "a") as log:
-			writer = csv.writer(log, delimiter=';')
-			writer.writerow(lst)
-	else:
-		with open(speedtest_log, "w") as log:
-			writer = csv.writer(log, delimiter=';')
-			writer.writerow(['Date', 'Time', 'Ping', 'Download', 'Upload'])
-			writer.writerow(lst)
-		
-
+    ``` Checks if the speedtest_log file exists, if it doesn't
+    it creates it and adds the header row labesl, if it does, 
+    it will append the result to the end adding a new row. The
+    CSV delimiter is set to be ';' which works well with Excel```
+    if os.path.isfile(speedtest_log):
+	with open(speedtest_log, "a") as log:
+            writer = csv.writer(log, delimiter=';')
+            writer.writerow(lst)
+    else:
+        with open(speedtest_log, "w") as log:
+	    writer = csv.writer(log, delimiter=';')
+	    writer.writerow(['Date', 'Time', 'Ping', 'Download', 'Upload'])
+	    writer.writerow(lst)
 
 csv_write(nettest())
